739b345313e208ccfaafc22d9eacd75a76e22818
David Törnros <david@dxgruppen.se>
2024-02-23 20:22:14 +0100

    added pokemon sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getPokemons } from "./services/service";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [pokemons, setPokemons] = useState();
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		const fetchData = async () => {
+			const pokemonData = await getPokemons();
+			setPokemons(pokemonData);
+			console.log(pokemonData);
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			{pokemons && pokemons.length > 0 ? (
+				pokemons.map((pokemon, index: number) => (
+					<div key={index}>
+						<p>{pokemon.name}</p>
+						<img
+							alt="pokemon"
+							src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}
+						/>
+					</div>
+				))
+			) : (
+				<p>No Pokemon data available</p>
+			)}
+		</>
+	);
 }
 
-export default App
+export default App;
